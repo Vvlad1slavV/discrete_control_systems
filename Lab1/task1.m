6707bb9be0ed88a94aa346cbfceda08ebf715420
Daniil Marukhlenko <daniil.marukh@gmail.com>
2022-09-03 13:15:38 +0300

    add plot funcs, move files

--- Lab1/task1.m
+++ Lab1/task1.m
@@ -1,8 +1,18 @@
+%% Init configs
+% model params
 config;
-
 K_oc = 1.2;
+
+% save img params
+task_number = 1;
+set_number = 1;
+path = "out/";
+
+%% Sim and save img
 sim_out = sim("model.slx", 2);
 time = sim_out.tout;
 y = sim_out.y;
 
-plot(time, y)
+plot_xy_discrete_lable(time, y,"$Time, s$","$Signal?$")
+saveas(gcf,path+'task'+string(task_number)+'_'+string(set_number)+'.png')
+close all;
