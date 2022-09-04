e1334df22e1eb19cf2ac713adf11e91cdbe9e1bb
Vlad1slav <Vvlad1slavV@yandex.ru>
2022-09-04 13:05:07 +0300

    Updare Lab1

--- Lab1/start_sim.m
+++ Lab1/start_sim.m
@@ -1,10 +1,8 @@
-function [tout,y] = start_sim(K_oc, K_oy, time)
-    K_oc = K_oc;
-    K_oy = K_oy;
+function [tout,y] = start_sim(K_oc_new, time)
+    assignin('base', 'K_oc', K_oc_new)
     sim_out = sim("model.slx", time);
 
     tout = sim_out.tout;
     y = sim_out.y;
-    k = sim_out.K;
 end
 
--- Lab1/task1.m
+++ Lab1/task1.m
@@ -3,16 +3,28 @@
 config;
 K_oc = 1.2;
 
+K_oc_arr = [1.0204, 0.515, 0.3, -0.2, 1.0204, 0.8, 1.2, 0.535];
+% Tau = 
+
 % save img params
 task_number = 1;
 set_number = 1;
 path = "out/";
 
 %% Sim and save img
-sim_out = sim("model.slx", 2);
-time = sim_out.tout;
-y = sim_out.y;
+for k = K_oc_arr
+    [time, y] = start_sim(k, 2);
+    plot_xy_discrete_lable(time, y,"$Time, s$","$Signal?$")
+    saveas(gcf, path+'task'+string(task_number)+'_'+string(set_number)+'_'+'koc'+string(k)+'.png')
+    set_number = set_number + 1;
+    close all;
+end
 
-plot_xy_discrete_lable(time, y,"$Time, s$","$Signal?$")
-saveas(gcf,path+'task'+string(task_number)+'_'+string(set_number)+'.png')
-close all;
+%% Save model as pdf
+print('-smodel', '-dpdf', 'out/model')
+
+
+%%
+[time, y] = start_sim(0.535, 2);
+stairs(time, y)
+100*(max(y) - y(50))
